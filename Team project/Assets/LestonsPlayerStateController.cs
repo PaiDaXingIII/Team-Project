05c1dc098d1d247de6bcdbd33e32fe75aa24ba98
Leston123456 <2115537494@qq.com>
2025-04-15 15:25:56 +0800

    Make some changes to the scene
    Make some changes to the scene

---
+++ Team project/Assets/LestonsPlayerStateController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LestonsPlayerStateController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forward = Input.GetKey("w");
+
+        if (forward) 
+        {
+            animator.SetBool("walking", true);
+
+        }
+    }
+}
