585ee4cc9e2b7a61d7c22ba7a6fc088c8f184181
Leston123456 <2115537494@qq.com>
2025-04-18 22:02:43 +0800

    Add sound effects
    Add sound effects

--- Team project/Assets/LuoSong/Scripts/LuoSong Scene.cs
+++ Team project/Assets/LuoSong/Scripts/LuoSong Scene.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LestonsTmenu : MonoBehaviour
+public class LuoSongScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void Jump()
+    {
+        SceneManager.LoadScene(5);
     }
 }
